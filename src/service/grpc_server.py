ee6cf7174728ba24fc40b6ab6577cce29986255e
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2018-03-09 13:12:25 +0900

    define default addr and port number

--- src/service/grpc_server.py
+++ src/service/grpc_server.py
@@ -11,10 +11,17 @@ class DockerMigrator(docker_migration_pb2_grpc.DockerMigratorServicer):
     def SayHello(self, request, context):
         return docker_migration_pb2.HelloReply(message='Hello, %s!' % request.name)
 
-def serve():
+"""
+Start gRPC server based on given addr and port number.
+
+@params String addr
+@params Integer port
+"""
+def serve(addr, port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
     docker_migration_pb2_grpc.add_DockerMigratorServicer_to_server(DockerMigrator(), server)
-    server.add_insecure_port('[::]:50051')
+    addr_with_port = addr + ':' + str(port)
+    server.add_insecure_port(addr_with_port)
     server.start()
     try:
         while True:
