b8614fba93f8eed98c90e93892512a834b9db652
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2018-04-17 14:06:27 +0900

    formalize Sending messages

--- src/tool/socket_client/remote_com_client.py
+++ src/tool/socket_client/remote_com_client.py
@@ -1,5 +1,18 @@
 import socket
-import configparser
+from enum import Enum
+
+"""
+Message Type for sending a socket server
+refer to: https://github.com/westlab/door_vnf_platform/blob/master/platform_manager/service_api/headers_API/client_man_shared.hpp#L17-L18
+"""
+class ClientMessageCode(Enum):
+    BUF_LOCATION = 0
+    RULE_INS = 1
+    RULE_DEL = 2
+    ACK = 3
+    MSG_WAIT = 4
+    MSG_OTHER = 5
+    SIG_CHG = 6
 
 class RemoteComClient:
 
@@ -8,8 +21,20 @@ class RemoteComClient:
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
     def connect(self):
-        s.connect(self.socket_path)
+        self.socket.connect(self.socket_path)
 
-    def formalize_message(self):
+    """
+    Formalize a message passing to vnf_platform
 
-
+    @params Integer app_id
+    @params Integer message_no
+    @params Integer message_type
+    @params String payload
+    @string message
+    """
+    def formalize_message(self, app_id, message_type, message_no=0, payload=''):
+        message = '{app_id},{no},{type},{payload}'.format(app_id=str(app_id),
+                                                          no=str(message_no),
+                                                          type=str(message_type),
+                                                          payload=payload)
+        return message
